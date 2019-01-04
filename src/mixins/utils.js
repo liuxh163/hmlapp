0e4460109f903c33379e3396d3dd6826cafa9e34
hnxylc8818 <victory1993>
2019-01-04 17:25:45 +0800

    完善登录逻辑和个人信息修改

--- src/mixins/utils.js
+++ src/mixins/utils.js
@@ -51,6 +51,23 @@ export default class utils extends wepy.mixin {
     }
   }
 
+  formatTime(date) {
+    var year = date.getFullYear()
+    var month = date.getMonth() + 1
+    var day = date.getDate()
+
+    var hour = date.getHours()
+    var minute = date.getMinutes()
+    var second = date.getSeconds()
+
+    return [year, month, day].map(this.formatNumber).join('-')
+  }
+
+  formatNumber(n) {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+  }
+
   // px转rpx
   px2rpx(px){
     return px * 2 + 'rpx';
