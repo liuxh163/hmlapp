f5831bd27488690019c571044a1fe3387cfcc8c2
hnxylc8818 <victory1993>
2019-01-04 15:51:18 +0800

    完善首页接口调用

--- src/utils/httpUrl.js
+++ src/utils/httpUrl.js
@@ -1,5 +1,7 @@
 let httpUrl = {
   "login":"/api/v1/user/login",
   "sendSmsCode":"/api/v1/users/sendSms",
+  "getPosts":"/api/v1/posts/:id",
+  "getProducts":"/api/v1/products",
 };
 export {httpUrl}
