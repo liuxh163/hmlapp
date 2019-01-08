6cd3c69432055fc24aeb1337c2e5e735a4336b36
徐宾 <xuzhibin212@gmail.com>
2019-01-08 15:13:15 +0800

    完善页面接口调用

--- src/utils/httpUrl.js
+++ src/utils/httpUrl.js
@@ -3,5 +3,6 @@ let httpUrl = {
   "sendSmsCode":"/api/v1/users/sendSms",
   "getPosts":"/api/v1/posts/:id",
   "getProducts":"/api/v1/products",
+  "modifyInfo":'/api/v1/users/:id',
 };
 export {httpUrl}
