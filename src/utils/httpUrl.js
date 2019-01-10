2713cf5da9bc1a568ada4ec7f9df6288d8b7d59a
hnxylc8818 <victory1993>
2019-01-10 21:11:19 +0800

    完善发现

--- src/utils/httpUrl.js
+++ src/utils/httpUrl.js
@@ -1,12 +1,14 @@
 let httpUrl = {
-  "login":"/api/v1/user/login",
-  "sendSmsCode":"/api/v1/users/sendSms",
-  "getPosts":"/api/v1/posts/:id",
-  "getProducts":"/api/v1/products",
-  "modifyInfo":'/api/v1/users',
-  "getPersonalList":'/api/v1/servants',
-  "getCarousels":"/api/v1/carousels",
-  "getMine":"/api/v1/users/mine",
-  "upload":"/api/v1/upload",
+  "login":"/api/v1/user/login",           // 登录
+  "sendSmsCode":"/api/v1/users/sendSms",  // 发送验证码
+  "getPosts":"/api/v1/topics/:id/posts",  // 查询帖子列表
+  "getPost":"/api/v1/posts",              // 查询指定帖子信息
+  "getProducts":"/api/v1/products",       // 查询产品列表
+  "modifyInfo":'/api/v1/users',           // 修改用户信息
+  "getPersonalList":'/api/v1/servants',   // 获取服务人员列表
+  "getCarousels":"/api/v1/carousels",     // 获取轮播图
+  "getMine":"/api/v1/users/mine",         // 获取用户信息
+  "upload":"/api/v1/upload",              // 上传文件
+  "sendComment":"/api/v1/comments",       // 发表评论
 };
 export {httpUrl}
