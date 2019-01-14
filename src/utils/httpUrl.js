85a9fea9a3afb5c73d35ce407d425e016d4d976f
徐宾 <xuzhibin212@gmail.com>
2019-01-14 18:59:54 +0800

    完善页面接口

--- src/utils/httpUrl.js
+++ src/utils/httpUrl.js
@@ -1,7 +1,7 @@
 let httpUrl = {
   "login":"/api/v1/user/login",           // 登录
   "sendSmsCode":"/api/v1/users/sendSms",  // 发送验证码
-  "posts":"/api/v1/topics/:id/posts",  // 查询帖子列表
+  "posts":"/api/v1/topics/:id/posts",     // 查询帖子列表
   "getPost":"/api/v1/posts",              // 查询指定帖子信息
   "getProducts":"/api/v1/products",       // 查询产品列表
   "modifyInfo":'/api/v1/users',           // 修改用户信息
@@ -9,6 +9,8 @@ let httpUrl = {
   "getCarousels":"/api/v1/carousels",     // 获取轮播图
   "getMine":"/api/v1/users/mine",         // 获取用户信息
   "upload":"/api/v1/upload",              // 上传文件
-  "sendComment":"/api/v1/comments",       // 发表评论
+  "comments":"/api/v1/comments",       // 发表评论
+  "getTags":"/api/v1/tags",               // 查询标签列表
+  "thumbs":"/api/v1/thumbs",              // 点赞
 };
 export {httpUrl}
